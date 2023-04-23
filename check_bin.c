32ebe57209a2236192408cf4112b85360bc41dd1
eyobelakew <Eyobelakew4@gmail.com>
2023-04-23 14:14:02 +0300

    Push initial source

--- check_bin.c
+++ check_bin.c
@@ -1,17 +1,22 @@
 #include "shell.h"
 
+/**
+ * checkbuild - searches for a built-in command
+ * @arv: array of arguments passed to the shell
+ * Return: function pointer to built-in command if found, NULL otherwise
+ */
 void (*checkbuild(char **arv))(char **arv)
 {
-
     static mybuild buildins[] = {
         {"env", env},
         {"exit", __exit},
         {"setenv", _setenv},
         {"unsetenv", _unsetenv},
-        {"cd", cd}, /* more built-in commands here */
+        {"cd", cd},
         {NULL, NULL}
     };
     int i;
+
     for (i = 0; buildins[i].name != NULL; i++)
     {
         if (strcmp(arv[0], buildins[i].name) == 0)
@@ -20,5 +25,4 @@ void (*checkbuild(char **arv))(char **arv)
         }
     }
     return NULL;
-    }
-
+}
