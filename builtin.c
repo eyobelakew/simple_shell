88f538c2b2a2d24ae6a682036a622516da22e824
ZorEl212 <yabsirad212@gmail.com>
2023-04-24 16:28:58 -0400

    Fix Betty

--- builtin.c
+++ builtin.c
@@ -6,14 +6,14 @@
  */
 void __exit(char **arv)
 {
-    if (arv[1])
-    {
-        exit(_atoi(arv[1]));
-    }
-    else
-    {
-        exit(0);
-    }
+	if (arv[1])
+	{
+		exit(_atoi(arv[1]));
+	}
+	else
+	{
+		exit(0);
+	}
 }
 
 /**
@@ -24,33 +24,33 @@ void __exit(char **arv)
  */
 void cd(char **arv)
 {
-    char *path = arv[1]; /* Retrive path from input array */
-    char *dir;
+	char *path = arv[1]; /* Retrive path from input array */
+	char *dir;
 
-    /* Check if path is null or "~". assign dir to HOME if so */
-    if (path == NULL || strcmp(path, "~") == 0)
-    {
-        dir = getenv("HOME");
-    }
-    else if (strcmp(path, "-") == 0) /* Set to prev dir if satisfied */
-    {
-        dir = getenv("OLDPWD");
-    }
-    else
-    {
-        dir = path; /*Else set dir to provided path */
-    }
+	/* Check if path is null or "~". assign dir to HOME if so */
+	if (path == NULL || strcmp(path, "~") == 0)
+	{
+		dir = getenv("HOME");
+	}
+	else if (strcmp(path, "-") == 0) /* Set to prev dir if satisfied */
+	{
+		dir = getenv("OLDPWD");
+	}
+	else
+	{
+		dir = path; /*Else set dir to provided path */
+	}
 
-    char *curr_dir = getcwd(NULL, 0); /* Get absolute path of current dir */
-    if (chdir(dir) != 0)
-    {
-        printf("cd: %s: No such file or directory\n", dir);
-    }
-    else
-    {
-        my_setenv("OLDPWD", curr_dir, 1);
-        my_setenv("PWD", getcwd(NULL, 0), 1);
-    }
+	char *curr_dir = getcwd(NULL, 0); /* Get absolute path of current dir */
+	if (chdir(dir) != 0)
+	{
+		printf("cd: %s: No such file or directory\n", dir);
+	}
+	else
+	{
+		my_setenv("OLDPWD", curr_dir, 1);
+		my_setenv("PWD", getcwd(NULL, 0), 1);
+	}
 }
 
 /**
@@ -59,13 +59,13 @@ void cd(char **arv)
  */
 void env(char **arv)
 {
-    int i;
+	int i;
 
-    /* loop through environ to print all variables */
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        printf("%s\n", environ[i]);
-    }
+	/* loop through environ to print all variables */
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
 }
 
 /**
@@ -76,19 +76,19 @@ void env(char **arv)
  */
 void _setenv(char **arv)
 {
-    if (arv[1] == NULL)
-    {
-        puts("Usage: setenv VAR VALUE");
-        return;
-    }
-    if (arv[2] == NULL)
-    {
-        my_unsetenv(arv[1]);
-    }
-    else
-    {
-        my_setenv(arv[1], arv[2], 1);
-    }
+	if (arv[1] == NULL)
+	{
+		puts("Usage: setenv VAR VALUE");
+		return;
+	}
+	if (arv[2] == NULL)
+	{
+		my_unsetenv(arv[1]);
+	}
+	else
+	{
+		my_setenv(arv[1], arv[2], 1);
+	}
 }
 
 /**
@@ -97,10 +97,10 @@ void _setenv(char **arv)
  */
 void _unsetenv(char **arv)
 {
-    if (arv[1] == NULL)
-    {
-        puts("Usage: unsetenv VAR");
-        return;
-    }
-    my_unsetenv(arv[1]);
+	if (arv[1] == NULL)
+	{
+		puts("Usage: unsetenv VAR");
+		return;
+	}
+	my_unsetenv(arv[1]);
 }
