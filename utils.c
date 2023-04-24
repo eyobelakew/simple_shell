75b9caadfd62091e88b60fac635f65ba82d44946
ZorEl212 <yabsirad212@gmail.com>
2023-04-24 17:08:40 -0400

    Helper functions for my_setenv func.

--- utils.c
+++ utils.c
@@ -1,81 +1,6 @@
 #include "shell.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
- *
- * Return: the converted integer
- */
-int _atoi(char *s)
-{
-	int res = 0, sign = 1, i = 0;
-
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i]; ++i)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10 + s[i] - '0';
-		}
-		else
-		{
-			break;
-		}
-	}
-	return (sign * res);
-}
-
-
-/**
- * splitstring - splits a string into an array of strings
- * @str: the string to split
- * @delim: the delimiter to use for splitting
- *
- * Return: a dynamically-allocated array of strings
- */
-char **splitstring(char *str, const char *delim)
-{
-	char **words = NULL;
-	char *token = NULL;
-	int i = 0;
-
-	/* allocate memory for the words array */
-	words = malloc(sizeof(char *));
-	if (words == NULL)
-		return (NULL);
-
-	/* split the string into words using the delimiter */
-	token = strtok(str, delim);
-	while (token != NULL)
-	{
-		/* resize the words array */
-		words = realloc(words, (i + 2) * sizeof(char *));
-		if (words == NULL)
-			return (NULL);
-
-		/* allocate memory for the current word and copy it */
-		words[i] = malloc(strlen(token) + 1);
-		if (words[i] == NULL)
-			return (NULL);
-		strcpy(words[i], token);
-
-		/* move to the next token */
-		token = strtok(NULL, delim);
-		i++;
-	}
-
-	/* add a NULL terminator to the end of the words array */
-	words[i] = NULL;
-
-	return (words);
-}
-
-
-/**
  * freearv - frees the array of pointers arv
  * @arv: array of pointers
  */
@@ -87,4 +12,92 @@ void freearv(char **arv)
 		free(arv[i]);
 
 	free(arv);
+}
+
+/**
+ * create_var - create a new environment variable string
+ *
+ * @name: name of the variable
+ * @value: value of the variable
+ *
+ * Return: pointer to the new variable string
+ */
+char *create_var(const char *name, const char *value)
+{
+	size_t name_len = strlen(name), value_len = strlen(value);
+	size_t var_len = name_len + 1 + value_len + 1;
+
+	char *new_var = malloc(var_len);
+
+	if (new_var == NULL)
+		return (NULL);
+
+	strncpy(new_var, name, name_len);
+	new_var[name_len] = '=';
+	strncpy(new_var + name_len + 1, value, value_len);
+	new_var[var_len - 1] = '\0';
+
+	return (new_var);
+}
+
+/**
+ * rep_var - replace an existing environment variable
+ *
+ * @name: name of the variable
+ * @new_var: pointer to the new variable string
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int rep_var(const char *name, char *new_var)
+{
+	char **p = environ;
+
+	while (*p)
+	{
+		size_t name_len = strlen(name);
+
+		if (strncmp(*p, name, name_len) == 0 && (*p)[name_len] == '=')
+		{
+			*p = new_var;
+			return (0);
+		}
+
+		p++;
+	}
+
+	return (-1);
+}
+
+/**
+ * add_var - add a new environment variable
+ *
+ * @new_var: pointer to the new variable string
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int add_var(char *new_var)
+{
+	size_t env_len = 0;
+	char **p = environ;
+
+	while (*p)
+	{
+		env_len++;
+		p++;
+	}
+
+	char **new_environ = malloc((env_len + 2) * sizeof(char *));
+
+	if (new_environ == NULL)
+	{
+		free(new_var);
+		return (-1);
+	}
+
+	memcpy(new_environ, environ, env_len * sizeof(char *));
+	new_environ[env_len] = new_var;
+	new_environ[env_len + 1] = NULL;
+	environ = new_environ;
+
+	return (0);
 }
