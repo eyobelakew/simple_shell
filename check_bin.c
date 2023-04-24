98933aa4440e767e59340ab8e17dcc6b24dc8903
ZorEl212 <yabsirad212@gmail.com>
2023-04-24 16:29:07 -0400

    Fix Betty

--- check_bin.c
+++ check_bin.c
@@ -7,22 +7,22 @@
  */
 void (*checkbuild(char **arv))(char **arv)
 {
-    static mybuild buildins[] = {
-        {"env", env},
-        {"exit", __exit},
-        {"setenv", _setenv},
-        {"unsetenv", _unsetenv},
-        {"cd", cd},
-        {NULL, NULL}
-    };
-    int i;
+	static mybuild buildins[] = {
+		{"env", env},
+		{"exit", __exit},
+		{"setenv", _setenv},
+		{"unsetenv", _unsetenv},
+		{"cd", cd},
+		{NULL, NULL}
+	};
+	int i;
 
-    for (i = 0; buildins[i].name != NULL; i++)
-    {
-        if (strcmp(arv[0], buildins[i].name) == 0)
-        {
-            return buildins[i].func;
-        }
-    }
-    return NULL;
+	for (i = 0; buildins[i].name != NULL; i++)
+	{
+		if (strcmp(arv[0], buildins[i].name) == 0)
+		{
+			return (buildins[i].func);
+		}
+	}
+	return (NULL);
 }
