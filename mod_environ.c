e5ed07444a5de0a59c830656bc10eee501c0346f
ZorEl212 <yabsirad212@gmail.com>
2023-04-24 16:29:29 -0400

    Fix Betty

--- mod_environ.c
+++ mod_environ.c
@@ -9,45 +9,48 @@
  */
 int my_unsetenv(const char *name)
 {
-    /* Check if the variable exists */
-    char *existing_value = getenv(name);
-    if (existing_value == NULL)
-    {
-        return (0);
-    }
+	/* Check if the variable exists */
+	char *existing_value = getenv(name);
 
-    /* Allocate memory for a new environment array without the variable */
-    int envc = 0;
-    while (environ[envc])
-    {
-        envc++;
-    }
-    char **new_environ = (char **)malloc((envc + 1) * sizeof(char *));
-    if (new_environ == NULL)
-    {
-        return (-1);
-    } /* Return failure if malloc fails */
+	if (existing_value == NULL)
+	{
+		return (0);
+	}
 
-    /* Copy the environment variables into the new array */
-    int i = 0;
-    while (environ[i])
-    {
-        if (strncmp(environ[i], name, strlen(name)) != 0 || environ[i][strlen(name)] != '=')
-        {
-            new_environ[i] = environ[i];
-        }
-        else /* Skip the variable to be unset */
-        {
-            i++;
-            continue;
-        }
-        i++;
-    }
-    new_environ[i] = NULL; /* Null terminate the array */
+	/* Allocate memory for a new environment array without the variable */
+	int envc = 0;
 
-    environ = new_environ; /* Replace environ with the modified one */
+	while (environ[envc])
+	{
+		envc++;
+	}
+	char **new_environ = (char **)malloc((envc + 1) * sizeof(char *));
+	if (new_environ == NULL)
+	{
+		return (-1);
+	} /* Return failure if malloc fails */
 
-    return (0);
+	/* Copy the environment variables into the new array */
+	int i = 0;
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], name, strlen(name)) != 0 || environ[i][strlen(name)] != '=')
+		{
+			new_environ[i] = environ[i];
+		}
+		else /* Skip the variable to be unset */
+		{
+			i++;
+			continue;
+		}
+		i++;
+	}
+	new_environ[i] = NULL; /* Null terminate the array */
+
+	environ = new_environ; /* Replace environ with the modified one */
+
+	return (0);
 }
 
 /**
@@ -61,62 +64,61 @@ int my_unsetenv(const char *name)
  */
 int my_setenv(const char *name, const char *value, int overwrite)
 {
-    /* Calculate the length of the new environment variable string */
-    size_t name_len = strlen(name);
-    size_t value_len = strlen(value);
-    size_t var_len = name_len + 1 + value_len + 1;
+	/* Calculate the length of the new environment variable string */
+	size_t name_len = strlen(name), value_len = strlen(value),var_len = name_len + 1 + value_len + 1;
 
-    /* Allocate memory for the new environment variable string */
-    char *new_var = (char *)malloc(var_len);
-    if (new_var == NULL)
-    {
-        return (-1);
-    }
+	/* Allocate memory for the new environment variable string */
+	char *new_var = (char *)malloc(var_len);
 
-    /* Construct the new environment variable string */
-    strncpy(new_var, name, name_len);
-    new_var[name_len] = '=';
-    strncpy(new_var + name_len + 1, value, value_len);
-    new_var[var_len - 1] = '\0';
+	if (new_var == NULL)
+		return (-1);
 
-    /* Search for the existing variable */
-    char **p = environ;
-    while (*p)
-    {
-        if (strncmp(*p, name, name_len) == 0 && (*p)[name_len] == '=')
-        {
-            if (overwrite)
-            {
-                /* If overwrite is set, replace the existing variable */
-                *p = new_var;
-                return (0);
-            }
-            else
-            {
-                /* If overwrite is not set, keep the existing variable */
-                free(new_var);
-                return (0);
-            }
-        }
-        p++;
-    }
+	/* Construct the new environment variable string */
+	strncpy(new_var, name, name_len);
+	new_var[name_len] = '=';
+	strncpy(new_var + name_len + 1, value, value_len);
+	new_var[var_len - 1] = NULL;
 
-    /* Variable not found, add it to the environment */
-    size_t env_len = p - environ;
-    char **new_environ = (char **)malloc((env_len + 2) * sizeof(char *));
-    if (new_environ == NULL)
-    {
-        free(new_var);
-        return (-1);
-    }
+	/* Search for the existing variable */
+	char **p = environ;
 
-    /* Copy the existing environment to the new environment */
-    memcpy(new_environ, environ, env_len * sizeof(char *));
-    new_environ[env_len] = new_var;
-    new_environ[env_len + 1] = NULL;
+	while (*p)
+	{
+		if (strncmp(*p, name, name_len) == 0 && (*p)[name_len] == '=')
+		{
+			if (overwrite)
+			{
+				/* If overwrite is set, replace the existing variable */
+				*p = new_var;
+				return (0);
+			}
+			else
+			{
+				/* If overwrite is not set, keep the existing variable */
+				free(new_var);
+				return (0);
+			}
+		}
+		p++;
+	}
 
-    /* Replace the old environment with the new environment */
-    environ = new_environ;
+	/* Variable not found, add it to the environment */
+	size_t env_len = p - environ;
+	char **new_environ = (char **)malloc((env_len + 2) * sizeof(char *));
 
-    return 0;
+	if (new_environ == NULL)
+	{
+		free(new_var);
+		return (-1);
+	}
+
+	/* Copy the existing environment to the new environment */
+	memcpy(new_environ, environ, env_len * sizeof(char *));
+	new_environ[env_len] = new_var;
+	new_environ[env_len + 1] = NULL;
+
+	/* Replace the old environment with the new environment */
+	environ = new_environ;
+
+	return (0);
 }
